17a5ee66049f32a2c909949ea991b9092cd606e0
GustavoTYX <130181989+GustavoTYX@users.noreply.github.com>
2023-08-13 09:59:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,21 +4,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
   generateButton.addEventListener("click", generateAndDisplayKey);
 
-  function generateAndDisplayKey() {
-    const storedKey = getStoredKeyFromServer();
+  async function generateAndDisplayKey() {
+    const storedKey = await getStoredKeyFromServer();
     generatedKeyElement.textContent = storedKey;
     generateButton.disabled = true;
   }
 
-  function getStoredKeyFromServer() {
+  async function getStoredKeyFromServer() {
     // Replace this with your actual API URL that serves the stored key
-    const apiUrl = "https://example.com/api/get-key.php";
+    const apiUrl = "https://github.com/GustavoTYX/Key_System/blob/main/store-key.php";
 
-    return fetch(apiUrl)
-      .then(response => response.text())
-      .catch(error => {
-        console.error("Error fetching stored key:", error);
-        return "-";
-      });
+    try {
+      const response = await fetch(apiUrl);
+      const storedKey = await response.text();
+      return storedKey;
+    } catch (error) {
+      console.error("Error fetching stored key:", error);
+      return "-";
+    }
   }
 });
